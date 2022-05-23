0f2d11bc4a02a61dfef4848fbe0314e3dc9da5d7
Findmebear <arm320@lehigh.edu>
2022-05-22 22:39:12 -0400

    saving work

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
+
+const data = [
+{
+  id: 1,
+  image: IMG1,
+  title: 'Crypto Currency Dashboard & Financial Vizualization',
+  github: 'https://github.com/Findmebear/personalWebsite', 
+  demo: 'https://dribbble.com/shots/17610643-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+},
+{
+  id: 2,
+  image: IMG2,
+  title: 'Crypto Currency Dashboard & Financial Vizualization',
+  github: 'https://github.com/Findmebear/personalWebsite', 
+  demo: 'https://dribbble.com/shots/17610643-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+},
+{
+  id: 3,
+  image: IMG3,
+  title: 'Crypto Currency Dashboard & Financial Vizualization',
+  github: 'https://github.com/Findmebear/personalWebsite', 
+  demo: 'https://dribbble.com/shots/17610643-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+}
+]
+
 const Portfolio = () => {
   return (
     <section id ='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key ={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt= {title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
     </section>
   )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import {Navigation, Pagination} from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Alibek Kaliyev',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tenetur recusandae culpa, impedit, commodi natus facere molestias harum facilis, assumenda itaque laboriosam non saepe tempora odio iste! Itaque, sed. Fugiat!'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Dr. Mark Erle',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tenetur recusandae culpa, impedit, commodi natus facere molestias harum facilis, assumenda itaque laboriosam non saepe tempora odio iste! Itaque, sed. Fugiat!'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Dr. Hank Korth',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tenetur recusandae culpa, impedit, commodi natus facere molestias harum facilis, assumenda itaque laboriosam non saepe tempora odio iste! Itaque, sed. Fugiat!'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Evan Greenberg',
+    review: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tenetur recusandae culpa, impedit, commodi natus facere molestias harum facilis, assumenda itaque laboriosam non saepe tempora odio iste! Itaque, sed. Fugiat!'
+  },
+]
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container" 
+            // install Swiper modules
+            modules={[Navigation, Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key ={index} className='testimonial'>
+                <div className='client__avatar'>
+                  <img src={avatar}/>
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
