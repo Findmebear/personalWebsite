ab26af2c026535b58903bc9d31b48d7d2670495e
Findmebear <arm320@lehigh.edu>
2022-06-10 18:22:35 -0400

    trying to deploy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,10 +7,10 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>EGATOR</a>
+      <a href="#header" className='footer__logo'>Artem Maryanskyy</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="#header">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
